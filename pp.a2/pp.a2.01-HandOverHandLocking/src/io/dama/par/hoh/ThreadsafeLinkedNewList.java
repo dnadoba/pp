b85295374e3576ec1064df5b6cf5dca93ca693d5
David Nadoba <dnadoba@gmail.com>
2017-12-25 16:03:51 +0100

    add lock to ListElement

--- pp.a2/pp.a2.01-HandOverHandLocking/src/io/dama/par/hoh/ThreadsafeLinkedNewList.java
+++ pp.a2/pp.a2.01-HandOverHandLocking/src/io/dama/par/hoh/ThreadsafeLinkedNewList.java
@@ -5,6 +5,7 @@ public class ThreadsafeLinkedNewList<T> implements NewList<T> {
         private T                    element;
         private ListElement<T>       prev;
         private final ListElement<T> next;
+        private final Lock 			 lock = new ReentrantLock(); 
 
         private ListElement(final T element, final ListElement<T> prev, final ListElement<T> next) {
             this.element = element;
@@ -40,6 +41,7 @@ public class ThreadsafeLinkedNewList<T> implements NewList<T> {
 
     @Override
     public synchronized void mod(final int i, final T e) {
+    	
         int j = 0;
         ListElement<T> ptr = this.first;
         while (j++ < i) {
