77babd827ba327c61e63fd123a6f7e9715b21090
David Nadoba <dnadoba@gmail.com>
2018-01-01 21:54:03 +0100

    implement `mod` method and add a root lock

--- pp.a2/pp.a2.01-HandOverHandLocking/src/io/dama/par/hoh/ThreadsafeLinkedNewList.java
+++ pp.a2/pp.a2.01-HandOverHandLocking/src/io/dama/par/hoh/ThreadsafeLinkedNewList.java
@@ -18,20 +18,24 @@ public class ThreadsafeLinkedNewList<T> implements NewList<T> {
     }
 
     private ListElement<T> first;
+    private final Lock rootLock = new ReentrantLock(); 
 
     public ThreadsafeLinkedNewList() {
         this.first = null;
     }
+    
 
     @Override
     public T get(final int i) {
         int j = 0;
+        rootLock.lock();
         ListElement<T> ptr = this.first;
         
         Lock currentLock = ptr.lock;
         Lock nextLock;        
 
     	currentLock.lock();
+    	rootLock.unlock();
         while (j++ < i) {
         	nextLock = ptr.next.lock;
         	nextLock.lock();
@@ -47,8 +51,9 @@ public class ThreadsafeLinkedNewList<T> implements NewList<T> {
     }
 
     @Override
-    public synchronized void add(final T e) {
+    public void add(final T e) {
     	final ListElement<T> insert = new ListElement<>(e, null, this.first);
+        rootLock.lock();
         
         if (this.first != null) {
         	this.first.lock.lock();
@@ -56,17 +61,36 @@ public class ThreadsafeLinkedNewList<T> implements NewList<T> {
             this.first.lock.unlock();
         }
         this.first = insert;   
+        
+        rootLock.unlock();
     }
 
     @Override
-    public synchronized void mod(final int i, final T e) {
+    public void mod(final int index, final T element) {
+    	if (index < 0) {
+    		throw new IndexOutOfBoundsException("Index must be greater than 0");
+    	}
     	
-        int j = 0;
-        ListElement<T> ptr = this.first;
-        while (j++ < i) {
-            ptr = ptr.next;
+    	rootLock.lock();
+    	ListElement<T> current = this.first;
+    	if(current == null) {
+    		throw new IndexOutOfBoundsException("Index " + index + " is out of bounds"); 
+    	}
+    	current.lock.lock();
+    	rootLock.unlock();
+        
+        for (int currentIndex = 0; currentIndex < index; currentIndex++) {
+        	ListElement<T> next = current.next;
+        	if(next == null) {
+        		throw new IndexOutOfBoundsException("Index " + index + " is out of bounds. Max index is " + currentIndex); 
+        	}
+        	next.lock.lock();
+        	current.lock.unlock();
+            current = next;
         }
-        ptr.element = e;
+        
+        current.element = element;
+        current.lock.unlock();
     }
 
 }
